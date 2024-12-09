fba57ed8a706066006ad81538fa143db3b7bd471
Caleb Kiptoo <vincentcalebkip@gmail.com>
2024-12-09 22:49:26 +0300

    DOM content LOaded

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function(){
+    const input = document.querySelector("#text")
+    input.textContent = "This is really cool!"
+})
